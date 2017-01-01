121983ba8c4c636725c0655668e85fe2042f04b6
Oliver Sõro <oliver.s6ro@gmail.com>
2017-01-02 01:42:55 +0200

    First implementation of sqld.

--- src/sqld/main.rs
+++ src/sqld/main.rs
@@ -1,8 +1,9 @@
 extern crate clap;
 use clap::{Arg, App};
 
-//extern crate rusqlite;
-//use rusqlite::Connection;
+extern crate rusqlite;
+use rusqlite::{Connection, Transaction, TransactionBehavior};
+use rusqlite::types::{ToSql};
 
 extern crate sqlcmdlutils;
 use sqlcmdlutils::dbpath::{DbPath, db_path_help, parse_db_path};
@@ -25,15 +26,76 @@ fn main() {
 
     let db_path = arg_matches.value_of("path").unwrap();
 
-//    let db_file = arg_matches.value_of("db_file").unwrap();
-//    let conn = Connection::open(db_file).unwrap();
+    let db_file = arg_matches.value_of("db_file").unwrap();
+    let mut conn = Connection::open(db_file).unwrap();
 
     match parse_db_path(db_path) {
         DbPath::Error(msg) => println!("{}", msg),
-        DbPath::Root => println!("To be implemented for root"),
-        DbPath::Table(table) => println!("To be implemented for table {}", table),
-        DbPath::TableColumns {table, columns} => println!("To be implemented for {} and {}", table, columns),
-        DbPath::TableFilter {table, filter} => println!("To be implemented for {} and {}", table, filter),
-        DbPath::TableColumnsFilter {table, columns, filter} => println!("To be implemented for {}, {} and {}", table, columns, filter),
+
+        DbPath::Root => {
+            let  tx = Transaction::new(&mut conn, TransactionBehavior::Deferred).unwrap();
+            let mut tables: Vec<String> = Vec::new();
+
+            { // prepare available tables
+                let query = "SELECT tbl_name FROM sqlite_master WHERE type = 'table' AND length(sql) > 0";
+                let mut stmt = tx.prepare(query).unwrap();
+                let mut rows = stmt.query(&[]).unwrap();
+                while let Some(result_row) = rows.next() {
+                    match result_row {
+                        Ok(row) => tables.push(row.get::<i32, String>(0)),
+                        Err(_) => ()
+                    }
+                }
+            }
+
+            if tables.len() > 0 {
+                // TODO Confirmation propmt
+                let mut tx_commit = true;
+                for t in tables {
+                    let delete_query = format!("DELETE FROM {}", t);
+                    match tx.execute(delete_query.as_str(), &[]) {
+                        Ok(deleted) => println!("Deleted {} row(s) from {}", deleted, t),
+                        Err(msg) => {
+                            println!("Delete failed at table: {} - doing rollback ...", t);
+                            println!("Delete failure message: {}", msg);
+                            tx_commit = false;
+                            break;
+                        }
+                    }
+                }
+                if tx_commit {
+                    match tx.commit() {
+                        Ok(_) => (),
+                        Err(_) => println!("Failed to commit deletes."),
+                    };
+                } else {
+                    match tx.rollback() {
+                        Ok(_) => (),
+                        Err(_) => println!("Failed to do rollback for deletes."),
+                    }
+                }
+            } else {
+                println!("No tables found in the database.");
+            }
+        }
+
+        DbPath::Table(table) => {
+            let query = format!("DELETE FROM {}", table);
+            execute_query(&conn, query.as_str(), table.as_str(), &[]);
+        }
+
+        DbPath::TableFilter {table, filter} => {
+            let query = format!("DELETE FROM {} WHERE {}", table, filter);
+            execute_query(&conn, query.as_str(), table.as_str(), &[]);
+        }
+
+        _ => println!("Column selection is not supported for DELETE."),
+    }
+}
+
+fn execute_query(conn: &Connection, query: &str, table: &str, params: &[&ToSql]) -> () {
+    match conn.execute(query, params) {
+        Ok(deleted) => println!("Deleted {} row(s) from {}", deleted, table),
+        Err(msg) => println!("Delete failed: {}", msg),
     }
 }
